b4fbac72cd632af48fb15d04c72a1e2358bed031
EC2 Default User <ec2-user@ip-172-31-93-182.ec2.internal>
2020-02-05 20:49:53 +0000

    part 2 checkbox

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,7 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
-    @all_ratings = ['G','PG','PG-13','R']
+    @all_ratings = {'G'=> 1,'PG'=> 1,'PG-13'=> 1,'R'=> 1}
     if params[:ratings]
       @movies = Movie.where(rating: params[:ratings].keys)
     end
